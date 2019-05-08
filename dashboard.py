c088caa856e40ed3e98cc8e9f7066362d948ab0b
Tim <thaines.astro@gmail.com>
2019-05-08 18:16:25 -0500

    Add basic upload page with template

--- dashboard.py
+++ dashboard.py
@@ -1,22 +1,8 @@
 from bottle import route, run, template
 
-@route('/hello/<name>')
-def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
+@route('/upload')
+def show_upload_form():
+    return template('upload')
 
-run(host='localhost', port=8080)
-
-
-# sub parse_log {
-#     open my $fdIn, '<', $_[0] or die "$_[0]: $!\n";
-#     my @results;
-#     while(<$fdIn>) {
-#         chomp;
-#         my @x = unpack('a27 a7 a5 a4 a9 a8 a8 a8 a23');
-#         my $status = pop @x;
-#         $status = (split(' ', $status))[0];
-#         push @x, $status;
-#         push @results, map {s/\s+//g; $_;} @x;
-#     }
-#     return @results;
-# }
+if __name__ == '__main__':
+    run(host='localhost', port=8080)
