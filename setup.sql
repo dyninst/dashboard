6e8397ba3009c831f39e6d8faa6492dd67754258
Tim <thaines.astro@gmail.com>
2019-05-08 18:10:37 -0500

    Add script for setting up the basic database structure

---
+++ setup.sql
@@ -0,0 +1,48 @@
+--create table link_type("name" varchar(12));
+--insert into link_type(name) values('dynamic');
+--insert into link_type(name) values('static');
+--
+--create table status("name" varchar(8));
+--insert into status(name) values('FAILED');
+--insert into status(name) values('CRASHED');
+--insert into status(name) values('SKIPPED');
+--insert into status(name) values('PASSED');
+--
+--create table mode("name" varchar(12));
+--insert into mode(name) values('attach');
+--insert into mode(name) values('create');
+--insert into mode(name) values('disk');
+--insert into mode(name) values('rewriter');
+
+--create table run(
+--	"id" INTEGER NOT NULL PRIMARY KEY,
+--	"arch" TEXT,
+--	"os" TEXT,
+--	"kernel" TEXT,
+--	"kernel_version" TEXT,
+--	"libc" TEXT,
+--	"hostname" TEXT,
+--	"build_status" TEXT,
+--	"date" DATE,
+--	"dyninst_commit" TEXT,
+--	"dyninst_branch" TEXT,
+--	"testsuite_commit" TEXT,
+--	"testsuite_branch" TEXT,
+--	"upload_file" TEXT
+--);
+
+--create table test_result(
+--	"resultid" INTEGER NOT NULL PRIMARY KEY,
+--	"runid" INTEGER NOT NULL,
+--	"test_name" TEXT,
+--	"compiler" TEXT,
+--	"optimization" TEXT,
+--	"abi" TEXT,
+--	"mode" TEXT,
+--	"threading" TEXT,
+--	"link" TEXT,
+--	"pic" TEXT,
+--	"result" TEXT,
+--	"reason" TEXT,
+--	FOREIGN KEY(runid) references run(id)
+--);
