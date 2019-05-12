0cc9c9ab1a09cc90f4f72f2268aae6fe114b66c7
Tim <thaines.astro@gmail.com>
2019-05-12 17:43:24 -0500

    Complete implementation of sql.inserts._insert_single

--- sql/inserts.py
+++ sql/inserts.py
@@ -1,13 +1,15 @@
 import sqlite3
+import csv
 
-def insert_single(table, fields, values):
+def _insert_single(table, fields, values):
+    """ Not for external consumption """
     if len(fields) != len(values):
         raise "Invalid insert into {0:s}\n {1:s}\n {2:s}\n".format(table,','.join(fields),','.join(values))
 
-    query = 'INSERT INTO {0:s}({1:s}) VALUES ({2:s})'.format(table,fields,'?'*len(values))
+    query = "INSERT INTO {0:s}({1:s}) VALUES ({2:s})".format(table,','.join(fields),','.join(['?']*len(values)))
     db = sqlite3.connect("test.sqlite3")
     cur = db.cursor()
-    cur.execute(query, '')
+    cur.execute(query, values)
     db.commit()
     rowid = cur.lastrowid
     cur.close()
