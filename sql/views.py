c5dd459d6fce69cc88689b2d17dbe7f60902c431
Tim <thaines.astro@gmail.com>
2019-05-12 17:46:25 -0500

    Add sql.views.regressions

--- sql/views.py
+++ sql/views.py
@@ -17,3 +17,41 @@ def results_summary(runid):
     res = cur.fetchall()
     cur.close()
     return res
+
+def regressions(new_runid, old_runid):
+    query = """
+        select
+            new.test,
+            new.comp,
+            new.opt,
+            new.abi,
+            new.mode,
+            new.thread,
+            new.link,
+            new.pic,
+            old.result as previous_result,
+            new.result as current_result
+        from
+            test_result as new
+            join test_result as old on
+                new.test = old.test
+                and new.comp = old.comp
+                and new.opt = old.opt
+                and new.abi = old.abi
+                and new.mode = old.mode
+                and new.thread = old.thread
+                and new.link = old.link
+                and new.pic = old.pic
+        where
+            new.runid = ?
+            and old.runid = ?
+            and new.result in('CRASHED','FAILED')
+            and new.result <> old.result
+    """
+    
+    db = sqlite3.connect("test.sqlite3")
+    cur = db.cursor()
+    cur.execute(query, str(new_runid), str(old_runid))
+    res = cur.fetchall()
+    cur.close()
+    return res
