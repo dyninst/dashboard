b4ebc8d1ed3fe635785c24fe27a7de91ac9285f8
Tim <thaines.astro@gmail.com>
2019-06-11 17:24:25 -0500

    Correctly handle base_url when not running under Bottle server

--- dashboard.py
+++ dashboard.py
@@ -7,18 +7,12 @@ from sql.bottle_sqlite import SQLitePlugin
 sqlite = SQLitePlugin(dbfile="results.sqlite3")
 bottle.install(sqlite)
 
-# This is here to allow running under the built-in bottle server
-base_url = ''
-
-@bottle.route('/regressions')
-def show_regressions(db):
-    cur_id = bottle.request.query.id
-    try:
-        regs = regressions.by_arch(db, cur_id)
-    except:
-        msg = str(sys.exc_info()[1])
-        raise bottle.HTTPError(500, 'Error calculating regressions: {0:s}'.format(msg))
-    return bottle.template('regressions', regs=regs, base_url=base_url)
+def get_base_url():
+    # This is here to allow running under the built-in bottle server
+    if __name__ == '__main__':
+        return ''
+    else:
+        return bottle.app().get_url('/')
 
 @bottle.route('/')
 def index(db):
@@ -28,7 +22,17 @@ def index(db):
         msg = str(sys.exc_info()[1])
         raise bottle.HTTPError(500, 'Error getting runs: {0:s}'.format(msg))
 
-    return bottle.template('runs', runs=res, base_url=base_url)
+    return bottle.template('runs', runs=res, base_url=get_base_url())
+
+@bottle.route('/regressions')
+def show_regressions(db):
+    cur_id = bottle.request.query.id
+    try:
+        regs = regressions.by_arch(db, cur_id)
+    except:
+        msg = str(sys.exc_info()[1])
+        raise bottle.HTTPError(500, 'Error calculating regressions: {0:s}'.format(msg))
+    return bottle.template('regressions', regs=regs, base_url=get_base_url())
 
 @bottle.route('/logs/<filename:path>')
 def download(filename):
@@ -36,7 +40,7 @@ def download(filename):
 
 @bottle.route('/upload')
 def show_upload_form():
-    return bottle.template('upload', base_url=base_url)
+    return bottle.template('upload', base_url=get_base_url())
 
 @bottle.route('/upload', method='POST')
 def process_upload(db):
@@ -47,9 +51,7 @@ def process_upload(db):
         msg = str(sys.exc_info()[1])
         raise bottle.HTTPError(500, 'Error processing upload: {0:s}'.format(msg))
     
-    return bottle.redirect(base_url)
+    return bottle.redirect(get_base_url())
 
 if __name__ == '__main__':
     bottle.run(host='localhost', port=8080)
-else:
-    base_url = bottle.default_app().get_url('/')
