8508dc487cffefa3a104dc913827ac3f2f298382
Tim <thaines.astro@gmail.com>
2019-06-17 18:05:32 -0500

    Allow SQL exceptions to escape test_results.summary

--- test_results.py
+++ test_results.py
@@ -1,14 +1,12 @@
 import sql.test_results
 
 def summary(db, runid):
-    try:                    
-        summary = {}
-        summary.setdefault('TOTAL', 0)
-        for k,v in sql.test_results.summary(db, runid):
-            summary.setdefault(k, v)
-            summary['TOTAL'] += v
-    except:
-        raise "Error creating summary"
+    summary = {}
+    summary.setdefault('TOTAL', 0)
+    for k,v in sql.test_results.summary(db, runid):
+        summary.setdefault(k, v)
+        summary['TOTAL'] += v
+
 
     if summary['TOTAL'] > 0:
         return \
