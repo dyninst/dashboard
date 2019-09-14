c94bd820de17e9eac4dba083c5fa076e6f983695
Tim Haines <thaines.astro@gmail.com>
2019-09-14 00:07:50 -0500

    Whitespace cleanup

--- regressions.py
+++ regressions.py
@@ -9,15 +9,16 @@ def by_arch(db, cur_id):
     regs = {}
     cur_run = sql.runs.get(db, runid=cur_id)[0]
     regs.setdefault('base_commit', cur_run)
-    
+
     regs['results'] = []
+    regs['type'] = 'arch'
     prev_runs = sql.runs.most_recent_by_arch(db, cur_id)
-    
+
     for r in prev_runs:
         d = {'run': r, 'regressions':None}
         regressions = sql.regressions.get(db, cur_id, r['id'])
         if len(regressions) > 0:
             d['regressions'] = regressions
         regs['results'].append(d)
-    
+
     return regs
