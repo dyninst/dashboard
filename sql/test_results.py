3e0f1e2657a52e57e9f5da8995b180b67f08c751
Tim Haines <thaines.astro@gmail.com>
2022-04-11 15:58:26 -0500

    Create summary table of test results

--- sql/test_results.py
+++ sql/test_results.py
@@ -1,19 +1,15 @@
-def status_names(db):
-    cur = db.cursor()
-    cur.execute("select name from status")
-    res = cur.fetchall()
-    cur.close()
-    return res
-
 def counts(db_conn, runid):
     query = """
         select
-            result,
-            count(1) as cnt
-        from test_result
+            passed,
+            failed,
+            skipped,
+            crashed,
+            hanged
+        from
+            test_result_summary
         where
             runid = ?
-        group by result
         """
     cur = db_conn.cursor()
     cur.execute(query, [str(runid)])
@@ -28,6 +24,14 @@ def bulk_insert(db_conn, runid, logfile):
         'abi', 'mode', 'threading',
         'link', 'pic', 'result', 'reason'
     ]
+    
+    counts = {
+        'PASSED':0,
+        'FAILED':0,
+        'SKIPPED':0,
+        'CRASHED':0,
+        'HANGED':0
+    }
 
     db_conn.execute('BEGIN TRANSACTION')
     for result in logfile:
@@ -36,7 +40,11 @@ def bulk_insert(db_conn, runid, logfile):
         if len(values) > 1:
             query = "INSERT INTO test_result({0:s}) VALUES ({1:s})".format(','.join(fields),','.join(['?']*len(values)))
             db_conn.execute(query, values)
+            counts[values[-2]] += 1
 
+    query = "INSERT INTO test_result_summary(runid,passed,failed,skipped,crashed,hanged) VALUES (?,?,?,?,?,?)"
+    db_conn.execute(query, [runid, counts['PASSED'], counts['FAILED'], counts['SKIPPED'], counts['CRASHED'], counts['HANGED']])
+    
     db_conn.commit()
 
 def get(db, runid, result=None):
--- test_results.py
+++ test_results.py
@@ -4,15 +4,14 @@ import sql.runs
 def summary(db, runid):
     summary = {}
     
-    # We want a count for each possible status
-    for row in sql.test_results.status_names(db):
-        summary[row['name']] = 0
-    summary['TOTAL'] = 0
+    names = ['PASSED','FAILED','SKIPPED','CRASHED','HANGED']
+    res = sql.test_results.counts(db, runid)
     
-    # Insert the actual counts (NB: not all possible statuses may be present)
-    for name,count in sql.test_results.counts(db, runid):
-        summary[name] = count
-        summary['TOTAL'] += count
+    for n,c in zip(names, res[0]):
+        summary[n] = c
+
+    summary['TOTAL'] = sum(summary.values())
+
     return summary
 
 def get(db, runid, result=None):
